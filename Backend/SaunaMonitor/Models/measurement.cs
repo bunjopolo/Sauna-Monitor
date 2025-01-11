a79051e2cceef4e84b298b15c7ff9ebe537354d8
bunjopolo <76890407+bunjopolo@users.noreply.github.com>
2025-01-11 15:37:42 -0800

    Changed temperature an humidity type to double

--- Backend/SaunaMonitor/Models/measurement.cs
+++ Backend/SaunaMonitor/Models/measurement.cs
@@ -3,9 +3,10 @@ namespace SaunaMonitor.Models
     public class Measurement
     {
         public int Id { get; set; }
-        public float TemperatureC { get; set; }
-        public float Humidity { get; set; }
+        public double TemperatureC { get; set; }
+        public double Humidity { get; set; }
         public DateTime ReadingDate { get; set; }
+        
         
     }
     
